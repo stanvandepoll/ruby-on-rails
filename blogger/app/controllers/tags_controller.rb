e9f7d97a6116f0aa1832b5b0c31a983a4a09257e
Stan van de Poll <swvandepoll@gmail.com>
2018-11-14 15:59:21 +0100

    Tagging feature completed, fixed with dependent: :destroy option for taggings in tag model.

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
     def show
         @tag = Tag.find(params[:id])
       end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+
+        flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+        redirect_to action: "index"
+    end
 end
